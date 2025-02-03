5654fd026e64677cf4502978d154de8db0076ad2
jayspatel21 <jayspatel2153@gmail.com>
2025-02-03 11:41:36 +0530

    update

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
   
   //reporter: [['html', { outputFolder: 'playwright-report' }]],
   reporter: process.env.CI ? 'blob' : 'html',
-  workers: 5, 
+  workers: 9, 
 
   /* Configure projects for major browsers */
   projects: [
